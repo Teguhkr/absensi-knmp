ab23d618b161c08a626c0680b91d5c7235341ea1
Teguhkr <teguhk356@gmail.com>
2026-05-22 16:48:18 +0700

    style: update Filament admin primary color and brand name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,9 +30,9 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Blue,
+                'primary' => Color::hex('#003F8A'),
             ])
-            ->brandName('KNMP Admin')
+            ->brandName('KNMP | Panel Admin')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
