15e9ba58928e7de591d06e1aeece636816041ddd
Teguhkr <teguhk356@gmail.com>
2026-06-23 01:38:40 +0700

    feat: add application favicon to layout and welcome pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Absensi - {{ \App\Models\PengaturanSistem::get('nama_instansi', 'KNMP') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo-knmp.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
